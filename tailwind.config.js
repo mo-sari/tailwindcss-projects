1b3d33aba0146066d04ef388d424790670615a89
mohsen ansari <mohsen.ansari.bim@gmail.com>
2025-04-19 11:44:41 +0330

    loopstudio project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
-      lg: "1020px",
+      lg: "976px",
       xl: "1440px",
     },
     extend: {
-      colors: {
-        softBlue: "hsl(231, 69%, 60%)",
-        softRed: "hsl(0, 94%, 66%)",
-        grayishBlue: "hsl(229, 8%, 60%)",
-        veryDarkBlue: "hsl(229, 31%, 21%)",
-      },
       fontFamily: {
-        sans: ["Rubik", "sans-serif"],
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
       },
-      backgroundImage: () => ({
-        dots: "url('../images/bg-dots.svg')",
-      }),
+      letterSpacing: {
+        widest: ".3em",
+      },
     },
   },
   plugins: [],
